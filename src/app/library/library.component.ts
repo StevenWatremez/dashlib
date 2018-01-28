554648640c52e68cd449b886a2dc3396f046f2c1
Steven Watremez <steven.watremez@niji.fr>
2018-01-28 18:22:20 +0100

    add style for this page

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -15,4 +15,34 @@ export class LibraryComponent implements OnInit {
   ngOnInit() {
   }
 
+  get isOld(): boolean {
+    return !this.upToDate(this.library.projectVersion, this.library.latestVersion) 
+  }
+  
+  upToDate(local: string, remote: string): boolean {
+    const VPAT = /^\d+(\.\d+){0,2}$/;
+
+    if (!local || !remote || local.length === 0 || remote.length === 0)
+      return false;
+    if (local == remote)
+      return true;
+    if (VPAT.test(local) && VPAT.test(remote)) {
+      var lparts = local.split('.');
+      while(lparts.length < 3)
+        lparts.push("0");
+      var rparts = remote.split('.');
+      while (rparts.length < 3)
+        rparts.push("0");
+      for (var i=0; i<3; i++) {
+        var l = parseInt(lparts[i], 10);
+        var r = parseInt(rparts[i], 10);
+        if (l === r)
+          continue;
+        return l > r;
+      }
+      return true;
+    } else {
+      return local >= remote;
+    }
+  }
 }
