49789c952ab1648f95a16e4885a6502824fdee21
Steven Watremez <steven.watremez@niji.fr>
2018-01-28 13:25:41 +0100

    add messaging with injection to log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LibrariesComponent } from './libraries/libraries.component';
 import { LibraryService } from './library.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibrariesComponent
+    LibrariesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [LibraryService],
+  providers: [LibraryService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
